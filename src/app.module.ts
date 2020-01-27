ce5e40514febf6dfb3098e1335ef193b0e5fe49e
EkimXD <miguelclementeaguilardiaz@hotmail.com>
2020-01-27 17:37:40 -0500

    add files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+      //aqui modulos
+    TypeOrmModule.forRoot(
+        {
+          name: 'default', // Nombre cadena de Conex.
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: '1234',
+          database: 'secondisall',
+          dropSchema: true,
+          entities: [
+              //aqui entities
+          ],
+          synchronize: true, // Crear -> true , Conectar -> false
+        },
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
